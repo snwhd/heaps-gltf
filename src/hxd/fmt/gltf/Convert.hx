a3f3bd730605096ad76b8d94bcb5893dc9b065a9
snwhd <none@none>
2024-01-22 21:03:22 -0800

    retab Convert.hx

--- src/hxd/fmt/gltf/Convert.hx
+++ src/hxd/fmt/gltf/Convert.hx
@@ -1,52 +1,52 @@
-package cerastes.fmt;
+package hxd.fmt.gltf;
 
 @:keep
-class ConvertGLTF2HMD extends hxd.fs.Convert
-{
-	var parseFunc : (String, String, haxe.io.Bytes)->cerastes.fmt.gltf.Data;
-	public function new(binary: Bool) {
-		this.parseFunc = binary ?
-			cerastes.fmt.gltf.Parser.parseGLB :
-			cerastes.fmt.gltf.Parser.parseGLTF;
+class ConvertGLTF2HMD extends hxd.fs.Convert {
 
-		super(binary?"glb":"gltf", "hmd");
-	}
+    var parseFunc : (String, String, haxe.io.Bytes) -> hxd.fmt.gltf.Data;
 
-	override function convert() {
+    public function new(binary: Bool) {
+        this.parseFunc = binary ?
+            cerastes.fmt.gltf.Parser.parseGLB :
+            cerastes.fmt.gltf.Parser.parseGLTF;
+        super(binary ? "glb" : "gltf", "hmd");
+    }
 
-		var splitPath = srcPath.split("/");
-		var name = splitPath[splitPath.length - 1];
+    override function convert() {
 
-		var localPath = srcPath.substr(0, srcPath.length-name.length);
+        var splitPath = srcPath.split("/");
+        var name = splitPath[splitPath.length - 1];
 
-		var relPath = "";
-		// Find the path relative to the assets dir
+        var localPath = srcPath.substr(0, srcPath.length-name.length);
 
-		var resPos = localPath.indexOf("/res/");
+        var relPath = "";
+        // Find the path relative to the assets dir
 
-		if (resPos != -1) {
-			relPath = localPath.substr(resPos+4);
-			trace(relPath);
-		}
-		try {
-			var gltfData = parseFunc(name, localPath, srcBytes);
-			var hmd = cerastes.fmt.gltf.HMDOut.emitHMD(name, relPath, gltfData);
-			var out = new haxe.io.BytesOutput();
-			new hxd.fmt.hmd.Writer(out).write(hmd);
-			save(out.getBytes());
-		}
-		catch( e : Dynamic ) throw Std.string(e) + " in " + srcPath;
+        var resPos = localPath.indexOf("/res/");
 
-	}
+        if (resPos != -1) {
+            relPath = localPath.substr(resPos+4);
+            trace(relPath);
+        }
+        try {
+            var gltfData = parseFunc(name, localPath, srcBytes);
+            var hmd = cerastes.fmt.gltf.HMDOut.emitHMD(name, relPath, gltfData);
+            var out = new haxe.io.BytesOutput();
+            new hxd.fmt.hmd.Writer(out).write(hmd);
+            save(out.getBytes());
+        }
+        catch( e : Dynamic ) throw Std.string(e) + " in " + srcPath;
 
-	static var glbConv = hxd.fs.Convert.register(new ConvertGLTF2HMD(true));
-	static var gltfConv = hxd.fs.Convert.register(new ConvertGLTF2HMD(false));
-	#if (sys || nodejs)
-	static var __ = hxd.fs.FileConverter.addConfig({
-		"fs.convert": {
-			"gltf" : { "convert" : "hmd", "priority" : -1 },
-		}
-	});
-	#end
+    }
+
+    static var glbConv = hxd.fs.Convert.register(new ConvertGLTF2HMD(true));
+    static var gltfConv = hxd.fs.Convert.register(new ConvertGLTF2HMD(false));
+    #if (sys || nodejs)
+    static var __ = hxd.fs.FileConverter.addConfig({
+        "fs.convert": {
+            "gltf" : { "convert" : "hmd", "priority" : -1 },
+        }
+    });
+    #end
 
 }
