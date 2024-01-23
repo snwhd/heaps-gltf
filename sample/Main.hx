5802bc214b0b9c3f302d017cfbc8ad5b467d1d48
snwhd <none@none>
2024-01-22 22:33:25 -0800

    fix up the sample

--- sample/Main.hx
+++ sample/Main.hx
@@ -12,16 +12,19 @@ class Main extends hxd.App {
         hxd.Res.initEmbed();
         var lights : h3d.scene.fwd.LightSystem = cast this.s3d.lightSystem;
         lights.ambientLight.set(1.0, 1.0, 1.0);
-        this.s3d.camera.pos.set(-10, -10, 10);
+        this.s3d.camera.pos.set(0.0, -0.25, 0.0);
         this.s3d.camera.target.set(0, 0, 0);
 
         var cache = new h3d.prim.ModelCache();
-        this.avocado = cache.loadModel(hxd.Res.Avocado);
+        this.avocado = cache.loadModel(hxd.Res.loader.load("Avocado.gltf").toModel());
+        this.avocado.rotate(Math.PI/2, 0, 0);
+        this.avocado.getChildAt(0).toMesh().material.shadows = false;
+        this.avocado.z = -0.035;
         this.s3d.addChild(avocado);
     }
 
     override function update(dt: Float) {
-        this.avocado.rotate(0, dt*Math.PI/4, 0);
+        this.avocado.rotate(0, 0, dt*Math.PI/4);
     }
 
 }
