d8f8a8bd9bf0dca1d97981e7d288a5baeeca6064
snwhd <none@none>
2024-01-29 00:18:06 -0800

    add config for glb, default to v1 parsing

--- src/hxd/fmt/gltf/Convert.hx
+++ src/hxd/fmt/gltf/Convert.hx
@@ -10,52 +10,27 @@ class ConvertGLTF2HMD extends hxd.fs.Convert {
     private var parseFunc: ParseFunction;
 
     public function new(binary: Bool) {
-        #if heaps_gltf_use_v1
+        #if heaps_gltf_use_v2
+        if (binary) throw "TODO: support glb";
+        super("gltf", "hmd");
+        #else
         this.parseFunc = binary ?
             hxd.fmt.gltf.Parser.parseGLB :
             hxd.fmt.gltf.Parser.parseGLTF;
         super(binary ? "glb" : "gltf", "hmd");
-        #else
-        if (binary) throw "TODO: support glb";
-        super("gltf", "hmd");
+        if (binary) trace("registering glb");
         #end
     }
 
     override function convert() {
-        #if heaps_gltf_use_v1
-        this.convertV1();
-        #else
+        #if heaps_gltf_use_v2
         this.convertV2();
+        #else
+        this.convertV1();
         #end
     }
 
-    #if heaps_gltf_use_v1
-
-    private function convertV1() {
-        var filename = haxe.io.Path.withoutDirectory(this.srcPath);
-        var filepath = haxe.io.Path.directory(this.srcPath);
-
-        // Find the path relative to the asset's dir
-        // TODO: rename to resdir or something
-        var directory = haxe.macro.Context.definedValue("resourcesPath");
-        if (directory == null) directory = "res";
-        var pos = this.srcPath.indexOf('/$directory/');
-        if (pos == -1) {
-            trace(filepath);
-            trace(this.srcPath);
-            throw "path not relative to resource dir?";
-        }
-        var relpath = filepath.substr(pos + directory.length + 2);
-
-        // read/parse gltf and output hmd
-        var data = this.parseFunc(filename, filepath, this.srcBytes);
-        var hmd = HMDOut.emitHMD(filename, relpath, data);
-        var out = new haxe.io.BytesOutput();
-        new hxd.fmt.hmd.Writer(out).write(hmd);
-        this.save(out.getBytes());
-    }
-
-    #else
+    #if heaps_gltf_use_v2
 
     private function convertV2() {
         var filename = haxe.io.Path.withoutDirectory(this.srcPath);
@@ -86,6 +61,32 @@ class ConvertGLTF2HMD extends hxd.fs.Convert {
         this.save(out.getBytes());
     }
 
+    #else
+
+    private function convertV1() {
+        var filename = haxe.io.Path.withoutDirectory(this.srcPath);
+        var filepath = haxe.io.Path.directory(this.srcPath);
+
+        // Find the path relative to the asset's dir
+        // TODO: rename to resdir or something
+        var directory = haxe.macro.Context.definedValue("resourcesPath");
+        if (directory == null) directory = "res";
+        var pos = this.srcPath.indexOf('/$directory/');
+        if (pos == -1) {
+            trace(filepath);
+            trace(this.srcPath);
+            throw "path not relative to resource dir?";
+        }
+        var relpath = filepath.substr(pos + directory.length + 2);
+
+        // read/parse gltf and output hmd
+        var data = this.parseFunc(filename, filepath, this.srcBytes);
+        var hmd = HMDOut.emitHMD(filename, relpath, data);
+        var out = new haxe.io.BytesOutput();
+        new hxd.fmt.hmd.Writer(out).write(hmd);
+        this.save(out.getBytes());
+    }
+
     #end
 
     // TODO: why is this hack needed
@@ -93,11 +94,12 @@ class ConvertGLTF2HMD extends hxd.fs.Convert {
     static var __ = hxd.fs.FileConverter.addConfig({
         "fs.convert": {
             "gltf": { "convert" : "hmd", "priority" : -1 },
+            "glb": { "convert" : "hmd", "priority" : -1 },
         }
     });
     #end
 
-    #if heaps_gltf_use_v1
+    #if !heaps_gltf_use_v2
     // TODO: support glb in v2
     static var glbConv = hxd.fs.Convert.register(new ConvertGLTF2HMD(true));
     #end
